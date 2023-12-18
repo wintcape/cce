2e5248abe3bc840ee0deab57ee5530bc7011c92e
wintcape <31993592+wintcape@users.noreply.github.com>
2023-12-17 22:53:07 -0500

    0.4.9

--- src/cce/application.c
+++ src/cce/application.c
@@ -757,8 +757,7 @@ cce_handle_user_input
         string_format ( ( *state ).textbuffer
                       , "%s"
                       , ( key == KEY_BACKSPACE ) ? "\b \b"
-                                                 : ( key != '[' ) ? ( char[] ){ key , 0 }
-                                                                  : ""
+                                                : ( char[] ){ key , 0 }
                       );
         RENDER ();
     }
